26d5d2f39b529f24e3ba8cd1b92bdb8734af5641
ulti m <multi@70-12-240-92.pools.spcsdns.net>
2016-05-16 14:37:07 +0900

    Initial Commit

---
+++ FractionCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FractionCalculator
+//
+//  Created by multi on 2016. 5. 16..
+//  Copyright © 2016년 multi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
